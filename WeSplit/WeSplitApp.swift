72721cf9aecb7a1372b4c58237ada41f7059ffd3
Aziz Baubaid <3zuccino@gmail.com>
2023-06-23 16:56:42 +0200

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Aziz Baubaid on 23.06.23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
